f4f6f5066c90425922a5d0805f387f9d1f95bce4
Deepak Singh <125148067+Deepak2723@users.noreply.github.com>
2023-08-02 22:48:31 +0530

    Add files via upload

---
+++ server/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+require("dotenv").config()
+
+const MONGODB_URL =process.env.MONGODB_URL|| "mongodb://localhost:27017/my_database";
+const connectDatabase = () => {
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then((data) => {
+      console.log(`Mongodb connected with server: ${data.connection.host}`);
+    }).catch((err)=>{
+        console.log("ERROR While connecting to DB",err.message)
+    })
+};
+
+module.exports = connectDatabase;
